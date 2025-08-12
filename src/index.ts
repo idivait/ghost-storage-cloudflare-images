a02eefe456ed1ee1ed40798d84bb9c1c58be78cb
idivait <10134142+idivait@users.noreply.github.com>
2025-08-12 12:17:56 -0500

    Add in basic docker setup for dev purposes.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { CloudflareImagesClient } from 'cloudflare-images-client'
+import { createReadStream } from "fs"
+import CloudflareImagesClient, { APIError } from 'cloudflare'
 import StorageBase, { type ReadOptions, type Image } from 'ghost-storage-base'
 
 type Config = {
@@ -32,42 +33,68 @@ class CloudflareStorage extends StorageBase {
   }
 
   async delete(fileName: string, targetDir?: string) {
+    console.warn(`filename`, fileName)
     return true;
   }
 
   async exists(fileName: string, targetDir?: string) {
+    console.warn(`filename`, fileName)
     return false;
   }
 
-  cloudflareConfig() {
-    const config = {
-        accountId: this.accountId || '',
-        apiToken: this.apiToken || '',
-    }
-    return config
+  cloudflare() {
+    return new CloudflareImagesClient({
+      apiToken: this.apiToken
+    })
   }
 
   isValidImage(image) {
     return image && image.name && image.path;
   }
+
   getCloudflareImageURL (id: string){
     return `https://imagedelivery.net/${this.accountHash}/${id}/public`
   }
 
+  getCloudflareImageID (url: string){
+    const regex = /https:\/\/imagedelivery.net\/(?<accounthash>.*)\/(?<id>.*)\/public/
+    const match = regex.exec(url)
+    if(!match?.groups?.id) return null
+    return match.groups.id
+  }
+
+  handleApiErrors (error: Error){
+    if (error instanceof CloudflareImagesClient.APIError) {
+      const apiError = error as APIError
+      console.log(apiError.status) // 400
+      console.log(apiError.name) // BadRequestError
+      console.log(apiError.headers) // {server: 'nginx', ...}
+    } else {
+      throw error
+    }
+  }
+
   async save(image: Image, targetDir?: string) {
+    console.warn(`image`, image)
     if(!this.isValidImage(image)) throw new Error('Invalid image object. Image must have name and path.')
 
     try {
-        const config = this.cloudflareConfig()
-        const client = new CloudflareImagesClient(config)
-        const upload = await client.uploadImageFromFile({
-            fileName: image.name,
-            filePath: image.path,
-            metadata: { ...image }
-        })
+        // const upload = await client.uploadImageFromFile({
+        //     fileName: image.name,
+        //     filePath: image.path,
+        //     metadata: { ...image }
+        // })
 
-        if (!upload.success || !upload.result) throw new Error(upload.errors.map(err=>err.message).join())
-        return this.getCloudflareImageURL(upload.result.id)
+        const upload = await this.cloudflare().images.v1.create({
+          account_id: this.accountId || '',
+          id: image.name,
+          file: createReadStream(image.path),
+          metadata: {...image},
+        }).catch(this.handleApiErrors)
+
+        if(!upload?.id) throw new Error(`No image id found. Image: ${image && JSON.stringify(image)}`)
+          
+        return this.getCloudflareImageURL(upload.id)
     } catch(err) {
         throw new Error(`Error during file save operation: ${err.message} `);
     }
